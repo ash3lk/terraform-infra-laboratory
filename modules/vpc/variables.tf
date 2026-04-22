cccff334e43d6d1ead14d19712f63a527743084f
ash3lk <shekoplyas12@gmail.com>
2026-04-22 17:20:52 +0500

    Update variables.tf in modules vpc block

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,1 +1,21 @@
+variable "env" {
+  type        = string
+  description = "dev, prod, infra"
+}
 
+variable "vpc_cidr" {
+  type        = string
+}
+
+variable "public_subnets" {
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  type        = list(string)
+}
+
+variable "azs" {
+  type    = list(string)
+  default = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
+}
