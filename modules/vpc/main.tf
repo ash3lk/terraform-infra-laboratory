2cbd923891925903d8116225bce25b3bebaaf001
ash3lk <shekoplyas12@gmail.com>
2026-04-22 17:07:13 +0500

    Update main.tf in modules block

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,1 +1,20 @@
+module "vpc_internal" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
 
+  name = "vpc-${var.env}"
+  cidr = var.vpc_cidr
+
+  azs             = var.azs
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Name        = "vpc-${var.env}"
+    Terraform   = "true"
+    Environment = var.env
+  }
+}
