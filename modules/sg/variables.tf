3904c2d5b772543beb77d654587b12a1c75234cf
ash3lk <shekoplyas12@gmail.com>
2026-04-23 12:51:52 +0500

    Create variables.tf for sg module

---
+++ modules/sg/variables.tf
@@ -0,0 +1,15 @@
+variable "env" {
+  type        = string
+  description = "dev, prod, infra"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "ID vpc for SG"
+}
+
+variable "allowed_ports" {
+  type        = list(number)
+  default     = [22, 80, 443]
+  description = "List of allowed ingress ports"
+}
