f4e78ba46d67c9e3abe304a8befaa19e6a39b5bf
ash3lk <shekoplyas12@gmail.com>
2026-04-23 13:34:28 +0500

    Update main.tf in dev environment

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,8 +1,19 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] 
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
 # 1. VPC
 module "vpc" {
   source          = "../../modules/vpc"
   env             = var.env
   vpc_cidr        = var.vpc_cidr
+  azs             = var.azs
   public_subnets  = var.public_subnets
   private_subnets = var.private_subnets
 }
@@ -11,17 +22,17 @@ module "vpc" {
 module "sg" {
   source        = "../../modules/sg"
   env           = var.env
-  vpc_id        = module.vpc.vpc_id # Берем ID из модуля VPC
-  allowed_ports = [22, 80, 443]      # SSH, HTTP, HTTPS
+  vpc_id        = module.vpc.vpc_id 
+  allowed_ports = var.allowed_ports
 }
 
-# 3. EC2 instance
+# 3. EC2
 module "ec2" {
   source    = "../../modules/ec2"
   env       = var.env
-  ami_id    = "ami-0fe8bec493a81c7da" # Ubuntu 22.04 LTS в eu-north-1
-  key_name  = "your-key-name"         # Имя твоего ключа из консоли AWS
+  ami_id    = data.aws_ami.ubuntu.id 
+  key_name  = var.key_name           
   
-  subnet_id = module.vpc.public_subnets[0] # Ставим в первую публичную подсеть
-  sg_id     = module.sg.sg_id              # Привязываем созданную SG
+  subnet_id = module.vpc.public_subnets[0] 
+  sg_id     = module.sg.sg_id              
 }
