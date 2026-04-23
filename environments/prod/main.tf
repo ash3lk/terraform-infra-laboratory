7b5ec23e82cf35d42268b2b80a149e12affcdf3d
ash3lk <shekoplyas12@gmail.com>
2026-04-23 17:44:47 +0500

    Update main.tf in prod

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,13 +1,8 @@
-module "vpc_prod" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "vpc-prod"
-  cidr = "10.1.0.0/16"
-
-  azs             = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
-  private_subnets = ["10.1.10.0/24", "10.1.11.0/24", "10.1.12.0/24"]
-  public_subnets  = ["10.1.20.0/24", "10.1.21.0/24", "10.1.22.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway  = true
+module "vpc" {
+  source          = "../../modules/vpc"
+  env             = "var.env"
+  vpc_cidr        = var.vpc_cidr
+  azs             = var.azs
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
 }
