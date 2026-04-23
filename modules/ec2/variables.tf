e5b447c92325a643704c94c6579c983ddc75d635
ash3lk <shekoplyas12@gmail.com>
2026-04-23 13:13:11 +0500

    Update variables.tf in ec2 module

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,1 +1,30 @@
+variable "env" {
+  type        = string
+  description = "Dev, prod, infra"
+}
 
+variable "ami_id" {
+  type        = string
+  description = "The AMI ID for Ubuntu 22.04"
+}
+
+variable "instance_type" {
+  type        = string
+  default     = "t3.micro"
+  description = "EC2 instance size"
+}
+
+variable "key_name" {
+  type        = string
+  description = "Name of the SSH key pair"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "Subnet ID where the instance will be launched"
+}
+
+variable "sg_id" {
+  type        = string
+  description = "Security Group ID to attach to the instance"
+}
