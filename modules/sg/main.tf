7f60048aceebffefa79255e2231aea0f751e80b9
ash3lk <shekoplyas12@gmail.com>
2026-04-27 17:06:02 +0500

    Update main.tf

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -26,4 +26,23 @@ resource "aws_security_group" "main_sg" {
     Name        = "sg-${var.env}"
     Environment = var.env
   }
+
+#SG for DB
+resource "aws_security_group" "rds_sg" {
+  name   = "${var.env}-rds-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ec2_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
