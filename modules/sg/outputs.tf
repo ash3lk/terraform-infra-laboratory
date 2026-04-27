25a62bf97b6f92c0abe1f5468fc139718234e455
ash3lk <shekoplyas12@gmail.com>
2026-04-27 17:40:30 +0500

    Update outputs.tf

--- modules/sg/outputs.tf
+++ modules/sg/outputs.tf
@@ -1,4 +1,9 @@
-output "sg_id" {
+output "ec_sg_id" {
   description = "Created security group ID"
   value       = aws_security_group.main_sg.id
+}
+
+output "rds_sg_id" {
+   description = "Created security group ID for RDS"
+  value = aws_security_group.rds_sg.id
 }
