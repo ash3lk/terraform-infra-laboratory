728f586a66564ec2d9447e4c4eed46ebd1758ca8
ash3lk <shekoplyas12@gmail.com>
2026-04-28 17:27:29 +0500

    Update variables.tf

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
-  description = "AWS Region where resources will be created"
   type        = string
   default     = "eu-north-1"
+  description = "AWS Region where resources will be created
 }
 
 variable "env" {
